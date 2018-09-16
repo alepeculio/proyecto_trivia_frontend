4784c4c4afd81d00ddd30141f5c3560aa7767c73
Jorge Rosas <rosasjorge1701@gmail.com>
2018-09-15 23:57:27 -0300

    Preguntas diarais 99%

--- src/PreguntasDiarias.js
+++ src/PreguntasDiarias.js
@@ -6,12 +6,12 @@ class PreguntasDiarias extends Component {
 		super();
 
 		let categorias = [
-			[ 'Geografía', { color: 'white', backgroundColor: 'blue' } ],
-			[ 'Historia', { color: 'white', backgroundColor: 'blue' } ],
-			[ 'Deportes', { color: 'white', backgroundColor: 'blue' } ],
-			[ 'Ciencia y Tecnología', { color: 'white', backgroundColor: 'blue' } ],
-			[ 'Arte y Literatura', { color: 'white', backgroundColor: 'blue' } ],
-			[ 'Entretenimiento', { color: 'white', backgroundColor: 'blue' } ]
+			[ 'Geografía', { color: 'white', backgroundColor: '#1575cf' } ],
+			[ 'Historia', { color: 'white', backgroundColor: '#a6911b' } ],
+			[ 'Deportes', { color: 'white', backgroundColor: '#ff9400' } ],
+			[ 'Ciencia y Tecnología', { color: 'white', backgroundColor: '#008639' } ],
+			[ 'Arte y Literatura', { color: 'white', backgroundColor: '#930309' } ],
+			[ 'Entretenimiento', { color: 'white', backgroundColor: '#e643a0' } ]
 		];
 
 		let preguntas = [];
@@ -23,20 +23,23 @@ class PreguntasDiarias extends Component {
 				preguntas.push( {
 					posicion: pos,
 					categoria: categorias[i][0],
-					estado: 'Nada',
+					estado: 'Cargando...',
 					estilo: categorias[i][1]
 				} );
 				pos++;
 			}
-
+		
 		this.state = {
-			preguntas: preguntas
+			preguntas: preguntas,
+			cantPregs: categorias.length * preguntasPorCategoria
 		};
 	}
 
-	generarPreguntaDiaria( posicion, categoria ) {
-		//5b930d67dd0a701d6889bf98
-		console.log( posicion + ", " + categoria );
+	generarPreguntaDiaria ( posicion, categoria ) {
+		let items = document.querySelectorAll( '#pdLista table tr' );
+		items[posicion - 1].querySelector( '.pdEstado' ).innerHTML = 'Respondiendo...';
+		items[posicion - 1].querySelector( '.pdEstado' ).removeAttribute( 'hidden' );
+		items[posicion - 1].querySelector( '.pdResponder' ).setAttribute( 'hidden', true );
 
 		fetch( 'http://localhost:1234/preguntas/generarPreguntaDiaria', {
 			method: 'POST',
@@ -49,28 +52,90 @@ class PreguntasDiarias extends Component {
 				posicion: posicion
 			} )
 		} ).then( res => {
-			console.log( res );
 			return res.json();
 		} ).then( pregunta => {
-			console.log( pregunta );
+			// TODO: Quiza quitar por rendimiento
+			this.cargarPreguntasDiarias();
 		} ).catch( err => {
 			console.log( err );
+		} );
+	}
+
+	componentDidMount () {
+		this.cargarPreguntasDiarias();
+	}
+
+	cargarPreguntasDiarias () {
+		fetch( 'http://localhost:1234/preguntas/preguntasDiarias?ID_Usuario=5b930d67dd0a701d6889bf98', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json; charset=utf-8'
+			}
+		} ).then( res => {
+			return res.json();
+		} ).then( preguntas => {
+			let items = document.querySelectorAll( '#pdLista table tr' );
+
+			for ( let i = 0; i < this.state.cantPregs; i++ ) {
+				items[i].querySelector( '.pdEstado' ).removeAttribute( 'hidden' );
+				items[i].querySelector( '.pdResponder' ).setAttribute( 'hidden', true );
+
+				let pregEncontrada = false;
+
+				for ( let j = 0; j < preguntas.length; j++ ) {
+					if ( preguntas[j].posicion === i + 1 ) {
+						let tdEstado = items[i].querySelector( '.pdEstado' );
+
+						let estado = preguntas[j].estado === 'NoRespondio' ? 'Incorrecta' : preguntas[j].estado;
+						tdEstado.innerHTML = estado;
+
+						tdEstado.classList.remove( 'correcta' );
+						tdEstado.classList.remove( 'incorrecta' );
+
+						if ( estado === 'Correcta' )
+							tdEstado.classList.add( 'correcta' );
+						else if ( estado === 'Incorrecta' )
+							tdEstado.classList.add( 'incorrecta' );
+
+						pregEncontrada = true;
+						preguntas.splice( j, 1 );
+						break;
+					}
+				}
+
+				if ( !pregEncontrada ) {
+					items[i].querySelector( '.pdEstado' ).setAttribute( 'hidden', true );
+					items[i].querySelector( '.pdResponder' ).removeAttribute( 'hidden' );
+				}
+			}
+		} ).catch( err => {
+			console.log( 'Error: ' + err );
 		} );
 	}
 
 	render () {
 		return (
 			<div id="pdLista">
-				{ this.state.preguntas.map( p => {
-					return (
-						<div class="pdPregunta" style = { p.estilo }>
-							<span class="pdPosicion">{ p.posicion }</span>
-							<span>{ p.categoria }</span>
-							<span class="pdEstado">{ p.estado }</span>
-							<button onClick = { () => { this.generarPreguntaDiaria( p.posicion, p.categoria ); } }>Responder</button>
-						</div>
-					);
-				} ) }
+				<table>
+					{ this.state.preguntas.map( p => {
+						return (
+							<tr>
+								<td>
+									{ p.posicion }
+								</td>
+								<td style = { p.estilo }>
+									{ p.categoria }
+								</td>
+								<td className="pdEstado">
+									{ p.estado }
+								</td>
+								<td className="pdResponder" style = { p.estilo } hidden onClick = { () => { this.generarPreguntaDiaria( p.posicion, p.categoria ) } }>
+									Responder
+								</td>
+							</tr>
+						);
+					} ) }
+				</table>
 			</div>
 		);
 	}
