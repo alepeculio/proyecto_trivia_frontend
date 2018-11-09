b76c3acaa840e6ee287c130aed6c6750cd80740c
alejandropeculio@gmail.com <alejandropeculio@gmail.com>
2018-11-09 19:21:28 -0300

    medallas

--- src/properties.js
+++ src/properties.js
@@ -1,14 +1,13 @@
-export const properties = {
+/*export const properties = {
 	ip: "https://triviatipserver.herokuapp.com",
 	puerto: "",
 	socket: ""
 };
-
+*/
 /*======== Descomentar para usar localmente =======================*/
-/*
+
 export const properties = {
-	ip: "http://localhost",
+	ip: "http://192.168.20.141",
 	puerto: ":1234",
 	socket: ":1234"
 };
-*/
